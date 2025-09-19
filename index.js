68d47d7d2a2b1c0d32d1fe75142d4c1cc4aee919
Erick Araújo <145768557+AraujoErickk@users.noreply.github.com>
2025-09-19 15:43:24 -0300

    Init commits

---
+++ index.js
@@ -0,0 +1,22 @@
+console.log("Arquivo index.js foi executado!");
+
+
+// index.js
+const crud = require('./pokemons');
+
+console.log("Pokemons iniciais:", crud.readAll());
+
+// CREATE
+crud.create("Pikachu");
+console.log("Depois de adicionar Pikachu:", crud.readAll());
+
+// READ
+console.log("Pokemon na posição 2:", crud.read(2));
+
+// UPDATE
+crud.update(1, "Mewtwo");
+console.log("Depois de atualizar índice 1:", crud.readAll());
+
+// DELETE
+crud.remove(0);
+console.log("Depois de remover índice 0:", crud.readAll());
---
+++ pokemons.js
@@ -0,0 +1,40 @@
+// pokemons.js
+// Array inicial com 5 pokemons
+let pokemons = ['Arceus', 'Giratina', 'Raikou', 'Rayquaza', 'Charizard'];
+
+// Create
+function create(name) {
+  pokemons.push(name);
+  return pokemons;
+}
+
+// Read
+function readAll() {
+  return pokemons;
+}
+
+function read(index) {
+  return pokemons[index] || null;
+}
+
+// Update
+function update(index, newName) {
+  if (pokemons[index]) {
+    const old = pokemons[index];
+    pokemons[index] = newName;
+    return { old, newName };
+  }
+  return null;
+}
+
+// Delete
+function remove(index) {
+  if (pokemons[index]) {
+    const deleted = pokemons.splice(index, 1);
+    return deleted[0];
+  }
+  return null;
+}
+
+// Exportando
+module.exports = { create, readAll, read, update, remove };
